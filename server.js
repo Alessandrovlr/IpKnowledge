148f071d0faebd6d5258da4d27cf50b1e8dbd465
Alessandro <aleluinisilva@gmail.com>
2025-03-26 23:12:42 -0300

    voltando mudanças da api

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ const buscarNoticias = async () => {
 
         const browser = await puppeteer.launch({
             headless: "new",
-            executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || "/usr/bin/google-chrome-stable",
             args: ["--no-sandbox", "--disable-setuid-sandbox"]
         });
         const page = await browser.newPage();
