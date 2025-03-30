bd19714ab942642430312eb41e937d51c82c578b
Alessandro <aleluinisilva@gmail.com>
2025-03-30 14:55:37 -0300

    correções nas noticias para exibir algo em situações diferentes (erro ou demora da api)

--- js/script.js
+++ js/script.js
@@ -3,31 +3,47 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function noticia() {
+    let titulos = document.querySelectorAll(".titulo-noticia");
+    let links = document.querySelectorAll(".link-noticia");
+
+    titulos.forEach(titulo => titulo.textContent = "Carregando notícias...");
+    links.forEach(link => {
+        link.textContent = "Aguarde...";
+        link.removeAttribute("href");
+    });
+
     fetch("https://api-noticias-gnub.onrender.com/noticias")
         .then(response => response.json())
         .then(noticias => {
-            console.log("Notícias recebidas:", noticias); 
+            console.log("Notícias recebidas:", noticias);
 
             
-            let titulos = document.querySelectorAll("#titulo-noticia");
-            let links = document.querySelectorAll("#link-noticia");
+            if (!Array.isArray(noticias) || noticias.length === 0) {
+                mostraErroNoticias()
+            }
 
-            console.log("Titulos encontrados:", titulos.length);
-            console.log("Links encontrados:", links.length);
-
-            
-            noticias.slice(0, 11).forEach((noticia, index) => {
+            noticias.slice(0, titulos.length).forEach((noticia, index) => {
                 if (titulos[index] && links[index]) {
                     titulos[index].textContent = noticia.titulo;
                     links[index].innerHTML = `<a href="${noticia.link}" target="_blank">Leia mais</a>`;
                 } else {
                     console.log("Não há elemento para notícia no índice", index);
+                    
                 }
             });
         })
         .catch(error => {
-            console.error("Erro ao buscar notícias:", error); 
+            console.error("Erro ao buscar notícias:", error);
+            mostraErroNoticias()
         });
+}
+
+function mostraErroNoticias() {
+    document.querySelectorAll(".titulo-noticia").forEach(titulo => titulo.textContent = "Erro ao carregar notícia.");
+    document.querySelectorAll(".link-noticia").forEach(link => {
+        link.textContent = "Tente novamente mais tarde.";
+        link.removeAttribute("href");
+    });
 }
 
 function toggleMenu() {
